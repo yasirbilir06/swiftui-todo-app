ed4dbd7249a7d56e9ead0cdde0f52183ebf90413
yasirbilir06 <yasiryenihasan@gmail.com>
2026-01-14 19:52:11 +0100

    Initial Commit

---
+++ DataUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DataUI
+//
+//  Created by Yasir Bilir on 14.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DataUI/DataUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataUIApp.swift
+//  DataUI
+//
+//  Created by Yasir Bilir on 14.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct DataUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
